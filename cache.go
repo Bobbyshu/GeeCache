464355ed269ca665967f0a3d81ff5781a42882f0
Bobbyshu <shu.yun@northeastern.edu>
2024-06-26 18:24:42 -0400

    feat(): encapsulate Get() with mutex

--- cache.go
+++ cache.go
@@ -19,9 +19,26 @@ func (c *cache) add(key string, value ByteView) {
 	c.mu.Lock()
 	// release lock after method
 	defer c.mu.Lock()
+	// lazy Initialization
+	// init it until use it
 	if c.lru == nil {
 		c.lru = lru.New(c.cacheBytes, nil)
 	}
 	// add cache
 	c.lru.Add(key, value)
+}
+
+func (c *cache) get(key string) (value ByteView, ok bool) {
+	c.mu.Lock()
+	defer c.mu.Lock()
+
+	if c.lru == nil {
+		return
+	}
+
+	if v, ok := c.lru.Get(key); ok {
+		return v.(ByteView), ok
+	}
+
+	return
 }
